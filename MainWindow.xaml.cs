98fcd6abfd5572b126cea06201edb4edbc56fbf8
181312024@stu.kmu.edu.tr <181312024@stu.kmu.edu.tr>
2021-07-23 00:16:52 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GirisWDPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeTitle();
+        }
+
+        void ChangeTitle() //bir metot tanımladık formun ismini değiştirmek için 
+        {
+            this.Title = "Doğan AY Hoşgeldiniz"; //form başlığına atadık
+
+        }
+    }
+}
